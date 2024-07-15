17ed3b097062ba218fbdcf024b7b2d42499495fc
Alcal Diaby <alcal.rs221@gmail.com>
2024-07-15 11:22:26 +0200

    "target" : "static" on nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -247,12 +247,6 @@ export default defineNuxtConfig({
 
   },
 
-  nitro: {
-    "output": {
-        dir: 'output',
-        serverDir: 'output/server',
-        publicDir: 'output/dist'
-    }
-},
+  "target" : "static"
 
 })
